bf6f0f8e69e4af3ea35afeb309b7bba71427b421
Serg-Kaptsan <s.kaptsan@ukr.net>
2023-07-27 13:14:58 +0300

    HW_10

---
+++ JS_10/hw_10.js
@@ -0,0 +1,31 @@
+// - Напишіть функцію яка рахує кількість букв великого регістру в стрінгу і повертає кількість CountLowrercase("abc"); ===> 0 CountLowrercase("abcABC123"); ===> 3
+
+window.onload = function() {
+
+    function countUppercase (str) {
+        let pattern = /[A-Z]/g;
+        let matches = str.match(pattern);
+        if (!matches) {
+        return 0;
+        }
+        return matches.length;
+    }
+    let inputStr = prompt ("Введіть значення:");
+    let result = countUppercase(inputStr);
+
+
+// - Написати функцію яка видаляє всі тексктові символи з стрінга, а отриманні цифрові значення переводить в тип намбер та повертає його clearString("hell5o wor6ld") ======> 56
+
+        let numMatch = (inputStr.match(/\d/g) || []).join('');
+  
+    console.log (`Кількість букв у верхньому регістрі: ${result}, ${numMatch ? 'числові значення: ' + numMatch : 'числових значень не знайдено'}`);
+
+
+    // - Створити функцію валідації юзернейма, імя може включати літери цифри, тире і нижьнє тире, довжина імя повина бути від 4 до 10 символів.
+
+    let userName = prompt("Введіть ім'я:");
+    let validateUsername = (/^[a-zA-Z0-9_-]{4,10}$/).test(userName);
+
+    console.log(validateUsername);
+
+}
